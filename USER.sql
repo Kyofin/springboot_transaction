3ac03d6720a6b9c53c1e0ad5ffb20d652bef20d3
huzekang <1040080742@qq.com>
2018-09-05 00:21:17 +0800

    添加sql脚本

---
+++ USER.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE test;
+
+USE test;
+
+CREATE TABLE t_user(
+  id BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(255) NOT NULL ,
+  password VARCHAR(255) NOT NULL ,
+  phone VARCHAR(255) NOT NULL
+) ENGINE=INNODB AUTO_INCREMENT=1000 DEFAULT CHARSET=utf8;
+
+
+INSERT INTO `test`.`t_user`(`id`, `name`, `password`, `phone`) VALUES (1001, '张三', '123', '18826071570');
